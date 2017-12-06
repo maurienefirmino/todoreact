9054c3f3a6e05bb17de1e8a8a0394cf5125f0de1
Mauriene <maurienefirmino@gmail.com>
2017-12-06 14:17:09 +0000

    tudo

---
+++ front-end/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós" />
+        
+        <h2>Nossa história</h2>
+        
+        respeita nossa história maluco
+    </div>
+    )
